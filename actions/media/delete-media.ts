24b94daad66e67a551f0113a0d649037726e0a15
Tabish Ali Shamsi <tabish.ashamsi@gmail.com>
2026-01-29 22:36:15 +0500

    feat(media): Add optional postId parameter to deleteMedia action for post context

--- actions/media/delete-media.ts
+++ actions/media/delete-media.ts
@@ -4,8 +4,10 @@ import checkAuth from "@/data/check-auth"
 import db from "@/lib/db"
 import imagekit from "@/lib/imagekit"
 import Media from "@/models/Media"
+import Post from "@/models/Post"
+import mongoose from "mongoose"
 
-export default async function deleteMedia(mediaId: string) {
+export default async function deleteMedia(mediaId: string, postId?: string) {
     try {
         const { id } = await checkAuth()
         await db()
@@ -21,6 +23,14 @@ export default async function deleteMedia(mediaId: string) {
         } catch (error) {
             console.error(error)
             return { error: "Something went wrong while deleting media" }
+        }
+
+        if (postId) {
+            const post = await Post.findById(postId)
+            if (post.media && post.media > 0) {
+                post.media = post.media.filter((p: mongoose.Types.ObjectId) => p.toString() !== mediaId)
+                await post.save()
+            }
         }
 
         await Media.findOneAndDelete({ _id: mediaId, authorId: id })
